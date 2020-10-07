1a691fa61357a617e968cfcaecb30ac0f1fc80e5
Sergey Manannikov <s_manannikov@mail.ru>
2020-10-07 13:11:38 +0300

    5.1. Зона видимости переменных.

--- src/main/java/ru/job4j/max/Reduce.java
+++ src/main/java/ru/job4j/max/Reduce.java
@@ -4,10 +4,10 @@ public class Reduce {
     private int[] array;
 
     public void to(int[] array) {
-        array = array;
+        this.array = array;
     }
 
-    public void print(int[] array) {
+    public void print() {
         for (int index = 0; index < array.length; index++) {
             System.out.println(array[index]);
         }
@@ -17,6 +17,6 @@ public class Reduce {
         int[] array = {1, 2, 3};
         Reduce reduce = new Reduce();
         reduce.to(array);
-        reduce.print(array);
+        reduce.print();
     }
 }
