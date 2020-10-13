2eaec5bb3a39799245fda7c40e18dd873c885eac
Sergey Manannikov <s_manannikov@mail.ru>
2020-10-14 00:10:28 +0300

    2. Scanner и чтение числа из консоли.

--- src/main/java/ru/job4j/io/Matches.java
+++ src/main/java/ru/job4j/io/Matches.java
@@ -3,23 +3,29 @@ package ru.job4j.io;
 import java.util.Scanner;
 
 public class Matches {
+
+    public static boolean valid(int m) {
+        return (m >= 1) && (m <= 3);
+    }
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int match = 11;
-        System.out.println("Перед Вами " + match + " спичек!");
-        System.out.println("За один ход можно взять от 1 до 3 спичек.");
+        int match = 11, count = 0;
+        System.out.println("Перед Вами " + match + " спичек!" + System.lineSeparator() +
+                "За один ход можно взять от 1 до 3 спичек.");
         while (match > 0) {
-            System.out.println("Сделайте Ваш ход: ");
+            if (count % 2 == 0) {
+                System.out.print("Игрок 1. Ваш ход: ");
+            } else System.out.print("Игрок 2. Ваш ход: ");
             int select = Integer.valueOf(input.nextLine());
-            if (select > 3) {
-                System.out.println("Вы можете взять не более 3 спичек за один ход!");
-            } else if (select < 1) {
-                    System.out.println("Вы должны взять минимум 1 спичку!");
-                } else {
+            if (valid(select)) {
                 match = match - select;
-            }
-            System.out.println("Осталось " + match + " спичек.");
+                System.out.println("Осталось " + match + " спичек.");
+                count++;
+            } else System.out.println("Вы можете взять не менее 1 и не более 3 спичек!");
         }
-        System.out.println("Вы выиграли!");
+        if (count % 2 == 0) {
+            System.out.println("Игрок 2 выиграл!");
+        } else System.out.println("Игрок 1 выиграл!");
     }
 }
