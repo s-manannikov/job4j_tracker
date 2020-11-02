9509841bd319521d508a421ce97fa18018080611
Sergey Manannikov <s_manannikov@mail.ru>
2020-11-02 11:44:58 +0300

    2. Отсортировать департаменты.

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -1,13 +1,29 @@
 package ru.job4j.collection;
 
-import java.util.Arrays;
 import java.util.Comparator;
-import java.util.List;
 
 public class DepDescComp implements Comparator<String> {
     @Override
     public int compare(String o1, String o2) {
-        int rsl;
+        String[] a = o1.split("/");
+        String[] b = o2.split("/");
+        int rsl = b[0].compareTo(a[0]);
+        if (rsl != 0) {
+            return rsl;
+        }
+        return o1.compareTo(o2);
+    }
+}
+
+
+
+
+
+
+
+
+
+/*        int rsl;
         List<String> a = Arrays.asList(o1.split("/"));
         List<String> b = Arrays.asList(o2.split("/"));
         if (a.get(0).equals(b.get(0))) {
@@ -18,3 +34,5 @@ public class DepDescComp implements Comparator<String> {
         return rsl;
     }
 }
+
+ */
