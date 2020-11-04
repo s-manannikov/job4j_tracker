d03377dfc66306d67a0228923e067003a131bb4f
Sergey Manannikov <s_manannikov@mail.ru>
2020-11-04 16:15:17 +0300

    3. Уникальность элементов и сортировка.

--- src/test/java/ru/job4j/stream/ProfilesTest.java
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -1,26 +1,37 @@
 package ru.job4j.stream;
 
 import junit.framework.TestCase;
+import org.junit.Before;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class ProfilesTest extends TestCase {
+    private List<Profile> profiles = new ArrayList<>();
+
+    @Before
+    public void setUp() {
+        profiles.add(new Profile(new Address("City", "City Street", 77, 7)));
+        profiles.add(new Profile(new Address("City", "City Street", 88, 8)));
+        profiles.add(new Profile(new Address("City", "City Street", 99, 9)));
+        profiles.add(new Profile(new Address("City", "City Street", 99, 9)));
+        profiles.add(new Profile(new Address("ACity", "City Street", 9, 99)));
+        profiles.add(new Profile(new Address("BCity", "City Street", 77, 7)));
+        profiles.add(new Profile(new Address("ACity", "City Street", 9, 99)));
+    }
 
     public void testCollect() {
         Profiles pr = new Profiles();
-        Profile profile1 = new Profile(new Address("City", "City Street", 77, 7));
-        Profile profile2 = new Profile(new Address("City", "City Street", 88, 8));
-        Profile profile3 = new Profile(new Address("City", "City Street", 99, 9));
-        List<Profile> profiles = List.of(profile1, profile2, profile3);
         List<Address> rsl = pr.collect(profiles);
-        List<Address> expected = List.of(
-                profile1.getAddress(),
-                profile2.getAddress(),
-                profile3.getAddress()
-        );
+        List<Address> expected = new ArrayList<>();
+        expected.add(new Address("ACity", "City Street", 9, 99));
+        expected.add(new Address("BCity", "City Street", 77, 7));
+        expected.add(new Address("City", "City Street", 77, 7));
+        expected.add(new Address("City", "City Street", 88, 8));
+        expected.add(new Address("City", "City Street", 99, 9));
         assertThat(rsl, is(expected));
     }
 }
