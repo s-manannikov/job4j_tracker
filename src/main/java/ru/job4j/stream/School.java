b126b16cd38837ba5b91a472c1005614aa521152
Sergey Manannikov <s_manannikov@mail.ru>
2020-11-04 14:56:31 +0300

    1. Фильтрация учеников.

--- src/main/java/ru/job4j/stream/School.java
+++ src/main/java/ru/job4j/stream/School.java
@@ -1,26 +1,13 @@
 package ru.job4j.stream;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class School {
     public List<Student> collect(List<Student> students, Predicate<Student> predicate) {
-        List<Student> stud = students.stream()
+        return students.stream()
                 .filter(predicate)
                 .collect(Collectors.toList());
-
-        return filter(students, predicate);
-    }
-
-    private static List<Student> filter(List<Student> students, Predicate<Student> predicate) {
-        List<Student> list = new ArrayList<>();
-        for (Student stud : students) {
-            if (predicate.test(stud)) {
-                list.add(stud);
-            }
-        }
-        return list;
     }
 }
